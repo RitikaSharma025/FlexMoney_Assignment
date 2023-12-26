6d0a73d2a4e36559aaaa387763cbfd83b82fc18b
Ritika Sharma <ritikasharma11154@gmail.com>
2023-12-26 22:56:44 +0530

    Update

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,11 +1,11 @@
 //JCNCqHrHysMjud8h -- atlas-password
-const express = require('express');
-const model = require('../models/user');
+const express = require("express");
+const model = require("../models/user");
 const User = model.User;
 const router = express.Router();
 
 // Route to create a new user
-router.post('/users', async (req, res) => {
+router.post("/users", async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
@@ -14,22 +14,41 @@ router.post('/users', async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
-router.post('/edit',async(req,res)=>{
-  try{
-      const user = req.body;
-      const find_user = await User.find({email:user.email});
-      
-      if(find_user.length)
-      {
-        const doc = await User.findByIdAndUpdate({_id:find_user[0]._id},user,{new:true});
-      res.status(201).send("Your Profile is Updated Successfully");
-      }
-      else
-      {
-        res.status(201).send("No User Found");
-      }
+router.post("/edit", async (req, res) => {
+  try {
+    const user = req.body;
+    const find_user = await User.find({ email: user.email });
 
-  }catch (error) {
+    if (find_user.length) {
+      const doc = await User.findByIdAndUpdate(
+        { _id: find_user[0]._id },
+        user,
+        { new: true }
+      );
+      res.status(201).send("Your Profile is Updated Successfully");
+    } else {
+      res.status(201).send("No User Found");
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+router.post("/delete", async (req, res) => {
+  try {
+    const user = req.body;
+    const find_user = await User.find({ email: user.email });
+
+    if (find_user.length) {
+      const doc = await User.findByIdAndDelete(
+        { _id: find_user[0]._id },
+        user,
+        { new: true }
+      );
+      res.status(201).send("Your Profile is Deleted Successfully");
+    } else {
+      res.status(201).send("No User Found");
+    }
+  } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
